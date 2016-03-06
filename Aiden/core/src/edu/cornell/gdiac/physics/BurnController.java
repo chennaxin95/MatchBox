a74d392940aae2d4ae3c24e6aaada03991cf8a5c
chennaxin95 <nc352@cornell.edu>
2016-03-05 23:00:41 -0500

    Updated comments

--- Aiden/core/src/edu/cornell/gdiac/physics/BurnController.java
+++ Aiden/core/src/edu/cornell/gdiac/physics/BurnController.java
@@ -2,6 +2,7 @@
  * Due to AidenController (previously platformController) integrates collision
  * controller and it's hard to single it out, I implemented the burning controller code
  * inside Update() and beginContact(). 
+ * AidenModel is modified with simple fuel system.
  * 
  * Also I have modified FlammableBlock's UpdateBurningState() to take in float since
  * counting with frame rates are less reliable than with time. 
