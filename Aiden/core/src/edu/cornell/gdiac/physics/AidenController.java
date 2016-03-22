8a913efce95666ba65406e7749064b90d755f840
wxy1224 <xw288@cornell.edu>
2016-03-22 00:44:40 -0400

    Lower the platform in 1st level

--- Aiden/core/src/edu/cornell/gdiac/physics/AidenController.java
+++ Aiden/core/src/edu/cornell/gdiac/physics/AidenController.java
@@ -208,7 +208,7 @@ public class AidenController extends WorldController
 	private static final float[][][] PLATFORMS = { {
 			{ 8.0f, 7.0f, 31.0f, 7.0f, 31.0f, 8.0f, 8.0f, 8.0f },
 			{ 1.0f, 12.0f, 10.0f, 12.0f, 10.0f, 13.0f, 1.0f, 13.0f },
-			{ 12.0f, 14.0f, 25.0f, 14.0f, 25.0f, 15.0f, 12.0f, 15.0f }
+			{ 12.0f, 13.0f, 25.0f, 13.0f, 25.0f, 14.0f, 12.0f, 14.0f }
 	}, { { 1.0f, 10.0f, 4.0f, 10.0f, 4.0f, 11.0f, 1.0f, 11.0f },
 			{ 3.0f, 5.0f, 7.0f, 5.0f, 7.0f, 6.0f, 3.0f, 6.0f },
 			{ 10.0f, 5.0f, 14.0f, 5.0f, 14.0f, 6.0f, 10.0f, 6.0f },
