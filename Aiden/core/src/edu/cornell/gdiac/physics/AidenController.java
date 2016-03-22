5ac95439982c3e42f8e6b35511edfa4b6c7f3f24
wxy1224 <xw288@cornell.edu>
2016-03-21 23:33:42 -0400

    Modify first level
    The fire has enough space to jump.

--- Aiden/core/src/edu/cornell/gdiac/physics/AidenController.java
+++ Aiden/core/src/edu/cornell/gdiac/physics/AidenController.java
@@ -192,10 +192,10 @@ public class AidenController extends WorldController
 	// Wall vertices
 	private static final float[][][] WALLS = { {
 			{ 1.0f, 0.0f, 31.0f, 0.0f, 31.0f, 1.0f, 1.0f, 1.0f },
-			{ 16.0f, 18.0f, 16.0f, 17.0f, 1.0f, 17.0f,
-					1.0f, 0.0f, 0.0f, 0.0f, 0.0f, 18.0f },
-			{ 32.0f, 18.0f, 32.0f, 0.0f, 31.0f, 0.0f,
-					31.0f, 17.0f, 16.0f, 17.0f, 16.0f, 18.0f } },
+			{ 16.0f, 22.0f, 16.0f, 21.0f, 1.0f, 21.0f,
+					1.0f, 0.0f, 0.0f, 0.0f, 0.0f, 22.0f },
+			{ 32.0f, 22.0f, 32.0f, 0.0f, 31.0f, 0.0f,
+					31.0f, 21.0f, 16.0f, 21.0f, 16.0f, 22.0f } },
 
 			{ { 1.0f, 0.0f, 31.0f, 0.0f, 31.0f, 1.0f, 1.0f, 1.0f },
 					{ 16.0f, 18.0f, 16.0f, 17.0f, 1.0f, 17.0f,
@@ -207,8 +207,8 @@ public class AidenController extends WorldController
 	/** The outlines of all of the platforms */
 	private static final float[][][] PLATFORMS = { {
 			{ 8.0f, 7.0f, 31.0f, 7.0f, 31.0f, 8.0f, 8.0f, 8.0f },
-			{ 1.0f, 12.0f, 9.0f, 12.0f, 9.0f, 13.0f, 1.0f, 13.0f },
-			{ 12.0f, 12.0f, 25.0f, 12.0f, 25.0f, 13.0f, 12.0f, 13.0f }
+			{ 1.0f, 12.0f, 10.0f, 12.0f, 10.0f, 13.0f, 1.0f, 13.0f },
+			{ 12.0f, 14.0f, 25.0f, 14.0f, 25.0f, 15.0f, 12.0f, 15.0f }
 	}, { { 1.0f, 10.0f, 4.0f, 10.0f, 4.0f, 11.0f, 1.0f, 11.0f },
 			{ 3.0f, 5.0f, 7.0f, 5.0f, 7.0f, 6.0f, 3.0f, 6.0f },
 			{ 10.0f, 5.0f, 14.0f, 5.0f, 14.0f, 6.0f, 10.0f, 6.0f },
@@ -775,7 +775,8 @@ public class AidenController extends WorldController
 		// canvas.begin();
 		canvas.begin(avatar.getX(), avatar.getY());
 		System.out.println(avatar.getX() + " " + avatar.getY());
-		canvas.draw(backGround, 0, 0);
+		//canvas.draw(backGround, 0, 0);
+		canvas.draw(backGround, new Color(1f,1f,1f,1f), 0f, 0f, canvas.getWidth(), canvas.getHeight()/18*22);
 		for (Obstacle obj : objects) {
 			obj.draw(canvas);
 		}
