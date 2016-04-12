0abc48ee68dfedff122c1aafcb36f9c292455c79
ats74 <ats74@cornell.edu>
2016-04-12 00:16:57 -0400

    Made some levels

--- Aiden/core/src/edu/cornell/gdiac/physics/editor/LevelEditor.java
+++ Aiden/core/src/edu/cornell/gdiac/physics/editor/LevelEditor.java
@@ -560,14 +560,14 @@ public class LevelEditor extends WorldController {
 		
 		ProjectModelJsonRep project = new ProjectModelJsonRep(aiden, blocks, npcs, goalDoor);
 		String project_str = json.prettyPrint(project);
-		String outputfile="Tutorial4.json";
+		String outputfile="Level2.json";
 		FileHandle file = Gdx.files.absolute(Gdx.files.getLocalStoragePath()+outputfile);
 		file.writeString(project_str, false);
 	}
 	
 	public void loadFromJson(){
 		System.out.println("Loading");
-		Scene scene=new Scene("Tutorial4.json");
+		Scene scene=new Scene("Level2.json");
 		reset();
 		System.out.println("Loading Characters");
 		for (CharacterModel ch:scene.getGuards()){
