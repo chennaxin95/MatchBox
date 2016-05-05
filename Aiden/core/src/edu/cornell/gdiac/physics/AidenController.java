06b7c3ae75a077284786efbfe43b6d92ee6da4a5
wxy1224 <xiaoyan@dhcp-gs-562.eduroam.cornell.edu>
2016-05-05 17:04:02 -0400

    remove one level

--- Aiden/core/src/edu/cornell/gdiac/physics/AidenController.java
+++ Aiden/core/src/edu/cornell/gdiac/physics/AidenController.java
@@ -918,30 +918,27 @@ public class AidenController extends WorldController
 			this.scene = new Scene("Tutorial3.json");
 			break;
 		case 4:
-			this.scene = new Scene("Easy1.json");
-			break;
-		case 5:
 			this.scene = new Scene("Easy2.json");
 			break;
-		case 6:
+		case 5:
 			this.scene = new Scene("Easy3.json");
 			break;
-		case 7:
+		case 6:
 			this.scene = new Scene("Med1.json");
 			break;
-		case 8:
+		case 7:
 			this.scene = new Scene("Med2.json");
 			break;
-		case 9:
+		case 8:
 			this.scene = new Scene("Level2.json");
 			break;
-		case 10:
+		case 9:
 			this.scene = new Scene("Level3.json");
 			break;
-		case 11:
+		case 10:
 			this.scene = new Scene("Level4.json");
 			break;
-		case 12:
+		case 11:
 			this.scene = new Scene("Hard1.json");
 			break;
 		default:
