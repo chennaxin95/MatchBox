8f8fb990340c9ec9e40dabf1f848afc851037c5e
wxy1224 <xiaoyan@dhcp-gs-562.eduroam.cornell.edu>
2016-05-05 13:18:12 -0400

    json parse checkpoint

--- Aiden/core/src/edu/cornell/gdiac/physics/scene/Scene.java
+++ Aiden/core/src/edu/cornell/gdiac/physics/scene/Scene.java
@@ -115,7 +115,10 @@ public class Scene implements SceneInterface {
 								new StoneBlock(x, y, 1.99f, 1.99f));
 					} else {
 						if (material.equals("fuel")) {
-							boolean icp = obj.getBoolean("isCheckpoint");
+							boolean icp = false;
+							if(obj.has("isCheckpoint")){
+								obj.getBoolean("isCheckpoint");
+							}
 							fuelBlocks.add(
 									new FuelBlock(x, y, 1/*b_scale_x*/,1/*b_scale_y*/,
 											burn_spread, burn_time, fuels,
